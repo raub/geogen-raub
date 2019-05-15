0c732f1a35f1850d757f2ac31ef28416c9c5f68c
Luis Blanco <luisblanco1337@gmail.com>
2019-05-15 22:21:04 +0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,193 @@
+# Geogen
+
+This is a part of [Node3D](https://github.com/node-3d) project.
+
+![NPM](https://nodei.co/npm/geogen-raub.png?compact=true)
+
+![Build Status](https://travis-ci.com/node-3d/geogen-raub.svg?branch=master)
+
+> npm i -s geogen-raub
+
+[Live DEMO](http://gsom.tech/geogen).
+
+
+## Synopsis
+
+Function `geogen` is exported.
+This is a planetary geometry generator. It generates a dynamic LOD chunked
+sphere with vertices displaced according to the values of the `heightmap`.
+The output is in form of raw number values, so it can be used with any
+graphical library.
+
+![Example](example.png)
+
+
+## Function `geogen`
+
+The idea is to generate a cube, and then normalize each of the vertices, resulting
+in a spheric geometry. Regarding the LODs, the approach is to subdivide any chunk
+into 4 equal pieces as soon as the LOD distance is reached.
+
+As the geometry is indexed and each chunk has the same setup, the same index buffer
+can be reused throughout all the chunks (being built only once).
+The time required for generation of chunks is also reduced by that fact.
+
+All the chunks have so called skirts. That is an additional geometry underneath
+the surface, designed to hide any LODding seams. So the geometry is ready for use.
+
+The texture coordinates are in planetary scale, [0; 1] for each side. However the
+directions are a bit tricky, and probably require some experimenting and additional
+docs. This snippet can probably help with texture generation, for now:
+```
+vec2 xy = varUv * (1024.0 + 1.0) - (512.0 + 0.5);
+vec3 coords;
+if (index == 0) {
+	coords = vec3(1024.0, -xy.y, xy.x);
+} else if (index == 1) {
+	coords = vec3(-1024.0, -xy.y, -xy.x);
+} else if (index == 2) {
+	coords = vec3(xy.x, 1024.0, -xy.y);
+} else if (index == 3) {
+	coords = vec3(-xy.x, -1024.0, -xy.y);
+} else if (index == 4) {
+	coords = vec3(-xy.y, xy.x, 1024.0);
+} else if (index == 5) {
+	coords = vec3(-xy.y, -xy.x, -1024.0);
+}
+```
+Here `index` is the side of the initial cube, as follows: [x, -x, y, -y, z, -z].
+
+
+### Args:
+
+```js
+{
+	radius: number = 500,
+	height: number = 100 / 255,
+	thresholds: [number] = [1300, 900, 700, 400, 200], // any length
+	resolution: number = 16, // int
+	heightmaps: [{
+		resolution: number = 1,
+		data: TypedArray = Uint8Array.from([0]),
+		at: number = 0,
+		step: number = 4,
+	}], // 6 pieces
+	onGeometry: GEOCALLBACK,
+}
+```
+
+* `radius` - base size of the sphere.
+* `height` - heightmap scale.
+* `thresholds` - a set of LOD distances.
+* `resolution` - the number of quads in a chunk (per side).
+* `heightmaps` - 6 heightmaps for each side of the sphere.
+* `onGeometry` - a callback to be called for each generated chunk, as described below.
+
+
+### Returns:
+
+```js
+{
+	update: () => {},
+}
+```
+
+* `update` - has to be called in order to recalculate the LOD.
+It can be called each frame or periodically.
+However, the more often it is called the less changes
+it has to manage, hence lower the latency.
+
+
+## Function `GEOCALLBACK`:
+
+This user function is responsible for interaction with the generator.
+The generator would call it each time a new chunk is created.
+
+The function receives all the generated buffers along with the side index and
+the chunk center initial position. This initial position can be used to later
+calculate the world-space chunk position required for LOD update.
+
+The function is expected to return several callbacks, that will be called during
+the LOD update. The LOD controller will need the actual information about the camera
+position, the chunk position and the position of the whole planet. The LOD
+controller will at some point decide to switch the visibility of a chunk ON or
+OFF, and expects the respective callback to do so.
+
+### Args:
+
+```js
+{
+	indices: Uint16Array,
+	vertices: Float32Array,
+	normals: Float32Array,
+	uvs: Float32Array,
+	index: number, // 0 1 2 3 4 5 - cube-side index
+	center: [number, number, number], // vec3
+}
+```
+
+* `indices` - index buffer.
+* `vertices` - vertex buffer.
+* `normals` - normal buffer.
+* `uvs` - texture coordinates buffer.
+* `index` - the side of the initial cube, as follows: [x, -x, y, -y, z, -z].
+* `center` - 3D position of the center of the chunk.
+
+### Returns:
+
+```js
+{
+	getChunkPos: () => [number, number, number],
+	getPlanetPos: () => [number, number, number],
+	getCameraPos: () => [number, number, number],
+	setVisible: v => {},
+}
+```
+
+* `getChunkPos` - get the current world position of the chunk.
+* `getPlanetPos` - get the current world position of the planet.
+* `getCameraPos` - get the current world position of the camera.
+* `setVisible` - show/hide the chunk.
+
+It is important to understand those are the functions you provide into the generator,
+so it can do a good job. If those functions are setup incorrectly, it won't work
+as expected.
+
+Here is an example of such a set of callbacks:
+
+```js
+return {
+	getChunkPos : () => {
+		const v3 = anchor.getWorldPosition(new THREE.Vector3());
+		return [v3.x, v3.y, v3.z];
+	},
+	getPlanetPos : () => {
+		const v3 = planet.position;
+		return [v3.x, v3.y, v3.z];
+	},
+	getCameraPos : () => {
+		const v3 = camera.position;
+		return [v3.x, v3.y, v3.z];
+	},
+	setVisible: v => {
+		surface.visible = v;
+	},
+};
+```
+
+In this example:
+
+* `anchor` - a THREE.Object3D earlier created at the `center` position.
+* `planet` - a THREE.Object3D representing the whole planet.
+* `camera` - a THREE.js camera.
+* `surface` - a THREE.Object3D representing this chunk.
+
+You dont have to nest chunk objects to enforce hierarchical invisibility.
+Nor that you could guess their nesting by the plain geometry values.
+The generator does that implicitly.
+
+---
+
+A working integration of this generator can be found on the
+[Live DEMO](http://gsom.tech/geogen) website. The heightmap here is generated
+with the help of [Texgen](https://github.com/node-3d/geogen-raub).
---
+++ index.js
@@ -0,0 +1,378 @@
+'use strict';
+
+
+(() => {
+	
+	const lerp1 = (a, b, t) => a + (b - a) * t;
+	const lerp = (a, b, t) => a.map((c, i) => lerp1(c, b[i]));
+	const length = v => Math.sqrt(v.reduce((s, c) => s + c * c, 0));
+	const scale = (v, f) => v.map(c => c * f);
+	const normalize = v => {
+		const len = length(v);
+		if (len > 0) {
+			return scale(v, 1 / len);
+		}
+		return v.slice();
+	};
+	const cross = ([ax, ay, az], [bx, by, bz]) => ([
+		ay * bz - az * by, az * bx - ax * bz, ax * by - ay * bx
+	]);
+	
+	const dot = (a, b) => a.reduce((s, c, i) => s + c * b[i], 0);
+	
+	const add = (a, b) => a.map((c, i) => c + b[i]);
+	const sub = (a, b) => a.map((c, i) => c - b[i]);
+	
+	const vertOrder = [[0, 0], [0, 1], [1, 0], [1, 1]];
+	
+	const createSide = opts => {
+		
+		const {
+			radius, height,
+			indices, resolution,
+			thresholds,
+			index, localUp,
+			heightmap,
+			vertBox, uvBox,
+			onGeometry,
+		} = opts;
+		
+		let subdivs    = null;
+		let isDetailed = false;
+		let isOffsite  = false;
+		
+		const thresholdOffset = thresholds[0] * 0.1;
+		
+		const {
+			resolution : heightmapRes = 1,
+			at         : heightmapAt = 0,
+			step       : heightmapStep = 4,
+			data       : heightmapData = Uint8Array.from([0]),
+		} = heightmap;
+		
+		const avgR  = radius + height * 0.5;
+		const axisA = [localUp[1], localUp[2], localUp[0]];
+		const axisB = cross(localUp, axisA);
+		
+		const resDivisor = 1 / resolution;
+		
+		const vertSize = sub(vertBox[1], vertBox[0]);
+		const uvSize   = sub(uvBox[1], uvBox[0]);
+		const vertStep = scale(vertSize, resDivisor * 0.5);
+		
+		// Landscape and skirts
+		const vertexNum = resolution * resolution + 2 * resolution + 1 + resolution * 4 + 4;
+		
+		const vertices = new Float32Array(vertexNum * 3);
+		const normals  = new Float32Array(vertexNum * 3);
+		const uvs      = new Float32Array(vertexNum * 2);
+		
+		const calcRel = (box, x, y) => [
+			lerp1(box[0][0], box[1][0], x * resDivisor),
+			lerp1(box[0][1], box[1][1], y * resDivisor),
+		];
+		
+		const getHeight = ([x, y]) => {
+			const iu = Math.min(heightmapRes - 1, Math.floor(x * heightmapRes));
+			const iv = Math.min(heightmapRes - 1, Math.floor(y * heightmapRes));
+			const idx = (iv * heightmapRes + iu) * heightmapStep + heightmapAt;
+			return heightmap.data[idx] * height;
+		};
+		
+		let occupied = 0;
+		const addVert = ({ uv, norm, vert }) => {
+			const current3f = occupied * 3;
+			const current2f = occupied * 2;
+			uvs.set(uv, current2f);
+			normals.set(norm, current3f);
+			vertices.set(vert, current3f);
+			occupied++;
+		};
+		
+		const calcNorm = (a, b) => normalize(
+			add(add(localUp, scale(axisA, a)), scale(axisB, b))
+		);
+		
+		for (let y = 0; y <= resolution; y++) {
+			for (let x = 0; x <= resolution; x++) {
+				const rvert = calcRel(vertBox, x, y);
+				const uv = calcRel(uvBox, x, y);
+				const norm = calcNorm(rvert[0], rvert[1]);
+				const vert = scale(norm, radius + getHeight(uv));
+				addVert({ uv, norm, vert });
+			}
+		}
+		
+		for (let x = 0; x <= resolution; x++) {
+			const rvert0 = lerp1(vertBox[0][0], vertBox[1][0], x * resDivisor);
+			const uv = calcRel(uvBox, x, 1);
+			const uv0 = calcRel(uvBox, x, 0);
+			const norm = calcNorm(rvert0, vertBox[0][1]);
+			const vert = scale(norm, (radius + getHeight(uv0)) * (1 - vertStep[1]));
+			addVert({ uv, norm, vert });
+		}
+		
+		for (let y = 0; y <= resolution; y++) {
+			const rvert1 = lerp1(vertBox[0][1], vertBox[1][1], y * resDivisor);
+			const uv = calcRel(uvBox, resolution - 1, y);
+			const uv0 = calcRel(uvBox, resolution, y);
+			const norm = calcNorm(vertBox[1][0], rvert1);
+			const vert = scale(norm, (radius + getHeight(uv0)) * (1 - vertStep[0]));
+			addVert({ uv, norm, vert });
+		}
+		
+		for (let x = resolution; x >= 0; x--) {
+			const rvert0 = lerp1(vertBox[0][0], vertBox[1][0], x * resDivisor);
+			const uv = calcRel(uvBox, x, resolution - 1);
+			const uv0 = calcRel(uvBox, x, resolution);
+			const norm = calcNorm(rvert0, vertBox[1][1]);
+			const vert = scale(norm, (radius + getHeight(uv0)) * (1 - vertStep[1]));
+			addVert({ uv, norm, vert });
+		}
+		
+		for (let y = resolution; y >= 0; y--) {
+			const rvert1 = lerp1(vertBox[0][1], vertBox[1][1], y * resDivisor);
+			const uv = calcRel(uvBox, 1, y);
+			const uv0 = calcRel(uvBox, 0, y);
+			const norm = calcNorm(vertBox[0][0], rvert1);
+			const vert = scale(norm, (radius + getHeight(uv0)) * (1 - vertStep[0]));
+			addVert({ uv, norm, vert });
+		}
+		
+		const vertCenter = add(vertBox[0], scale(vertSize, 0.5));
+		const center = scale(
+			normalize(add(
+				add(localUp, scale(axisA, vertCenter[0])),
+				scale(axisB, vertCenter[1])
+			)),
+			avgR
+		);
+		
+		const {
+			getChunkPos,
+			getPlanetPos,
+			getCameraPos,
+			setVisible,
+		} = onGeometry({ indices, vertices, normals, uvs, index, center });
+		
+		const getSubdivs = () => {
+			
+			if (subdivs) {
+				return subdivs;
+			}
+			
+			const thresholdsNew = thresholds.slice(1);
+			
+			const vertHalfSize = scale(vertSize, 0.5);
+			const uvHalfSize = scale(uvSize, 0.5);
+			
+			subdivs = [
+				{
+					vertBox: [vertBox[0], add(vertBox[0], vertHalfSize)],
+					uvBox: [uvBox[0], add(uvBox[0], uvHalfSize)],
+				},
+				{
+					vertBox: [sub(vertBox[1], vertHalfSize), vertBox[1]],
+					uvBox: [sub(uvBox[1], uvHalfSize), uvBox[1]],
+				},
+				{
+					vertBox: [
+						add(vertBox[0], [vertHalfSize[0], 0]),
+						[vertBox[1][0], vertBox[0][1] + vertHalfSize[1]],
+					],
+					uvBox: [
+						add(uvBox[0], [uvHalfSize[0], 0]),
+						[uvBox[1][0], uvBox[0][1] + uvHalfSize[1]],
+					],
+				},
+				{
+					vertBox: [
+						add(vertBox[0], [0, vertHalfSize[1]]),
+						[vertBox[0][0] + vertHalfSize[0], vertBox[1][1]],
+					],
+					uvBox: [
+						add(uvBox[0], [0, uvHalfSize[1]]),
+						[uvBox[0][0] + uvHalfSize[0], uvBox[1][1]],
+					],
+				},
+			].map(boxes => createSide(Object.assign({}, opts, boxes, {
+				thresholds: thresholdsNew,
+			})));
+			
+			return subdivs;
+			
+		};
+		
+		const vertMin = scale(calcNorm(vertBox[0][0], vertBox[0][1]), avgR);
+		const vertMax = scale(calcNorm(vertBox[1][0], vertBox[0][1]), avgR);
+		const sideStep = length(sub(vertMax, vertMin)) * 0.5;
+		
+		return {
+			setVisible: v => {
+				setVisible(v);
+				if (subdivs && ! v) {
+					subdivs.forEach(plane => plane.setVisible(false));
+				}
+			},
+			update() {
+				
+				const [threshold] = thresholds;
+				
+				if ( ! threshold ) {
+					return;
+				}
+				
+				const centerPos = getChunkPos();
+				const cameraPos = getCameraPos();
+				const planetPos = getPlanetPos();
+				
+				if (vertSize[0] < 1) {
+					
+					const localCenter = normalize(sub(centerPos, planetPos));
+					const localCamera = normalize(sub(cameraPos, planetPos));
+					const d = dot(localCenter, localCamera)
+					
+					if (d < 0 && ! isOffsite) {
+						setVisible(false);
+						isOffsite = true;
+						isDetailed = false;
+						getSubdivs().forEach(plane => {
+							plane.setVisible(false);
+						});
+					} else if (d > 0 && isOffsite) {
+						setVisible(true);
+						isOffsite = false;
+					}
+					
+					if (isOffsite) {
+						return;
+					}
+				}
+				
+				const dist = length(sub(centerPos, cameraPos)) - sideStep;
+				
+				if ( ! isDetailed && dist < threshold - thresholdOffset ) {
+					
+					setVisible(false);
+					isDetailed = true;
+					getSubdivs().forEach(plane => {
+						plane.setVisible(true);
+						plane.update();
+					});
+					
+				} else if (isDetailed && dist > threshold + thresholdOffset) {
+					
+					setVisible(true);
+					isDetailed = false;
+					getSubdivs().forEach(plane => {
+						plane.update();
+						plane.setVisible(false);
+					});
+					
+				} else if (isDetailed) {
+					
+					getSubdivs().forEach(plane => plane.update());
+					
+				}
+				
+			},
+		};
+		
+	};
+	
+	
+	const geogen = opts => {
+		
+		const emptyMap = { resolution: 1, data: Uint8Array.from([0]), at: 0, step: 4 };
+		
+		const defaultOpts = {
+			radius     : 500,
+			thresholds : [1300, 900, 700, 400, 200],
+			resolution : 16, // int
+			heightmaps : [emptyMap, emptyMap, emptyMap, emptyMap, emptyMap, emptyMap],
+		};
+		
+		const finalOpts = Object.assign({}, defaultOpts, opts);
+		
+		const { resolution } = finalOpts;
+		
+		// 6 = a quad. res^2 quads are the surface, and res*4 are the skirts
+		// const 4 extras are skirt edge duplicates, needed for correct uvs
+		const indexNum = 6 * resolution * (resolution + 4) + 4;
+		const indices  = new Uint16Array(indexNum);
+		
+		let ioccupied = 0;
+		const resolution1  = resolution + 1;
+		const resolution12 = resolution1 * resolution1;
+		
+		for (let y = 0; y < resolution; y++) {
+			for (let x = 0; x < resolution; x++) {
+				const i = y * resolution1 + x;
+				indices[ioccupied++] = i;
+				indices[ioccupied++] = i + resolution1 + 1;
+				indices[ioccupied++] = i + resolution1;
+				indices[ioccupied++] = i;
+				indices[ioccupied++] = i + 1;
+				indices[ioccupied++] = i + resolution1 + 1;
+			}
+		}
+		
+		const stripes = [
+			{ b: 0, k: 1 },
+			{ b: resolution, k: resolution1 },
+			{ b: resolution12 - 1, k: -1 },
+			{ b: resolution1 * resolution, k: -resolution1 },
+		].map(({ b, k }) => {
+			const stripe = [];
+			for (let x = 0; x < resolution1; x++) {
+				stripe.push(k * x + b);
+			}
+			return stripe;
+		}).map((stripe, i) => {
+			for (let x = 0; x < resolution1; x++) {
+				stripe.push(resolution12 + i * resolution1 + x);
+			}
+			return stripe;
+		}).forEach(stripe => {
+			for (let i = 0; i < resolution; i++) {
+				indices[ioccupied++] = stripe[i];
+				indices[ioccupied++] = stripe[i + resolution1];
+				indices[ioccupied++] = stripe[i + resolution1 + 1];
+				indices[ioccupied++] = stripe[i];
+				indices[ioccupied++] = stripe[i + resolution1 + 1];
+				indices[ioccupied++] = stripe[i + 1];
+			}
+		});
+		
+		const planes = [
+			[1, 0, 0], [-1, 0, 0],
+			[0, 1, 0], [0, -1, 0],
+			[0, 0, 1], [0, 0, -1],
+		].map((localUp, index) => {
+			const heightmap = finalOpts.heightmaps[index];
+			return createSide(Object.assign({}, finalOpts, {
+				index,
+				indices,
+				localUp,
+				heightmap,
+				vertBox : [[-1, -1], [1, 1]],
+				uvBox   : [[0, 0], [1, 1]],
+			}));
+		});
+		
+		return {
+			update() {
+				planes.forEach(plane => plane.update());
+			},
+		};
+		
+	};
+	
+	
+	if (typeof process !== 'undefined' && typeof module !== 'undefined' && module.exports) {
+		module.exports = geogen;
+	} else {
+		window.geogen = geogen;
+	}
+	
+})();
